77f3d3924681e620f39e883c2bb03eb54a436547
Bryan <bryanstevenal@gmail.com>
2021-01-29 16:05:48 +0100

    Añadir animación a menú

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,45 @@
 var barsMenu = document.getElementById('menu-hamburguesa');
 var menu = document.getElementById('menu');
+menu.style.right = '-100rem'
+
 var barsMenuIcon = document.getElementById('menu-hamburguesa-icon');
 
 const BARS_MENU_ICON = './img/menu.svg';
 const BARS_MENU_CLOSE_ICON = './img/cerrar.svg';
 
 barsMenu.addEventListener('click', () => {
-    menu.hidden = !menu.hidden;
-
-    if (!menu.hidden) {
-        barsMenuIcon.src = BARS_MENU_CLOSE_ICON;
+    
+    if (menu.style.right.includes('-100rem')) {
+        showMenu();
     } else {
-        barsMenuIcon.src = BARS_MENU_ICON;
+        hideMenu();
     }
+
 });
+
+function showMenu () {
+    var from = -100;
+    var id = setInterval(function(){
+        if (from == 0) {
+            barsMenuIcon.src = BARS_MENU_CLOSE_ICON;
+            clearInterval(id);
+        } else {
+            from = from + 2; 
+            menu.style.right = from + "rem"; 
+        }
+    }, 1);
+}
+
+function hideMenu () {
+    var from = 0;
+    var id = setInterval(function(){
+        if (from == -100) {
+            barsMenuIcon.src = BARS_MENU_ICON;
+            clearInterval(id);
+        } else {
+            from = from - 2; 
+            menu.style.right = from + "rem"; 
+        }
+    }, 1);
+}
+
